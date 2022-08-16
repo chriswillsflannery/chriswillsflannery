4ba338efdd97fd8d139fec84c34bad85d6a2ef77
cflannery <cflannery@williamhill.us>
2022-08-16 08:19:10 -0500

    add archive

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,16 +46,11 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered with{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>
+          <Link href="./archive">
+            Archive
+          </Link>
+        </p>
       </footer>
     </div>
   )
--- pages/posts/cssRenderingEngine2.tsx
+++ pages/posts/cssRenderingEngine2.tsx
@@ -97,23 +97,14 @@ const CSSRenderingEngine: NextPage = () => {
             alt="CSS Positioning"
           />
         </div>
-
-        <p>
-          <Link href="../">Back Home.</Link>
-        </p>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered with{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>
+          <Link href="../">
+            Home
+          </Link>
+        </p>
       </footer>
     </div>
   )
