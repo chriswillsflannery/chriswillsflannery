6ed48861c99101378cfbb653a9409eafac75feb8
cflannery <cflannery@williamhill.us>
2022-08-23 12:04:49 -0500

    fix path error

--- pages/posts/archive/binaryChristmasTree.tsx
+++ pages/posts/archive/binaryChristmasTree.tsx
@@ -3,10 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
 
-import browserStructure from '../../public/htmlRenderingEngine/1-browserStructure.jpg';
-import binaryChristmas from '../../public/binaryChristmas.jpeg';
+import binaryChristmas from '../../../public/binaryChristmas.jpeg';
 
-import styles from '../../styles/Home.module.css';
+import styles from '../../../styles/Home.module.css';
 
 const BinaryChristmasTree: NextPage = () => {
   return (
