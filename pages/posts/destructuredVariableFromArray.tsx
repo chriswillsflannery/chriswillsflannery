a9f3f0ebd27afcc43d3a09777a7dcb0177e80b70
Chris Flannery <chriswillsflannery@gmail.com>
2022-09-25 13:27:08 -0400

    use code blocks

--- pages/posts/destructuredVariableFromArray.tsx
+++ pages/posts/destructuredVariableFromArray.tsx
@@ -1,12 +1,80 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link';
+import Prism from 'prismjs';
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+
+import CodeBlock from '../components/CodeBlock';
 
 import styles from '../../styles/Home.module.css';
+import { useEffect } from 'react';
 
 const link = { color: 'darkSlateBlue' };
 
+const theBoys = `
+const theboys = ['butcher', 'hughie', 'soldier boy'];
+`;
+
+const firstElement = `
+const first = theboys[0];
+console.log(first);
+// butcher
+`;
+
+const emptyErray = `
+const vought = [];
+const first = vought[0];
+console.log(first);
+// undefined
+`;
+
+const destructure = `
+const theboys = ['butcher', 'hughie', 'soldier boy'];
+const [first] = theboys;
+console.log(first);
+// butcher
+`;
+
+const emptyArray2 = `
+const vought = [];
+const [first] = vought;
+console.log(first);
+// undefined
+`;
+
+const restParam = `
+const [first, ...rest] = theboys;
+console.log(first);
+// butcher
+console.log(rest);
+// [hughie, soldier boy]
+`;
+
+const mikeNorth = `
+const [firstUser] = db.getAllUsers();
+if (!firstUser) throw new Error();
+return firstUser;
+`;
+
+const useString = `
+const myfriend = 'jimmy';
+const [first, ...rest] = myfriend;
+console.log(first);
+// 'j'
+console.log(rest);
+// ['i','m','m','y']
+`;
+
 const DestructuredVariable: NextPage = () => {
+  useEffect(() => {
+    const highlight = async () => {
+      await Prism.highlightAll();
+    }
+    highlight();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -27,75 +95,31 @@ const DestructuredVariable: NextPage = () => {
         <article>
           <h4>Grab the first element of an array.</h4>
           <p>Say we create an array of variables, something like:</p>
-          <code>{`const theboys = ['butcher', 'hughie', 'soldier boy'];`}</code>
+          <CodeBlock code={theBoys} />
           <p>Then we want to grab the first element of the array (if it exists):</p>
-          <code>{`const first = theboys[0];`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// butcher`}</code>
+          <CodeBlock code={firstElement} />
           <p>What if the original array was empty?</p>
-          <code>{`const vought = [];`}</code>
-          <br />
-          <code>{`const first = vought[0];`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// undefined`}</code>
+          <CodeBlock code={emptyErray} />
         </article>
 
         <article>
           <h4>We can do the same thing with destructuring and the rest parameter:</h4>
-          <code>{`const theboys = ['butcher', 'hughie', 'soldier boy'];`}</code>
-          <br />
-          <code>{`const [first] = theboys;`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// butcher`}</code>
+          <CodeBlock code={destructure} />
           <p>Again, what if the original array was empty?</p>
-          <code>{`const vought = [];`}</code>
-          <br />
-          <code>{`const [first] = vought;`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// undefined`}</code>
+          <CodeBlock code={emptyArray2} />
           <p>Same end result.</p>
           <p>We can continue by accessing the &rdquo;rest&rdquo; of the elements of the array using the rest parameter:</p>
-          <code>{`const [first, ...rest] = theboys;`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// butcher`}</code>
-          <br />
-          <code>{`console.log(rest);`}</code>
-          <br />
-          <code>{`// [hughie, soldier boy]`}</code>
+          <CodeBlock code={restParam} />
         </article>
 
         <article>
           <h4>Mike North uses this in a <a style={link} href='https://frontendmasters.com/courses/fullstack-typescript/data-from-the-database/'>GraphQL Tutorial</a>, where he does something like this:</h4>
-          <code>{`const [firstUser] = db.getAllUsers();`}</code>
-          <br />
-          <code>{`if (!firstUser) throw new Error();`}</code>
-          <br />
-          <code>{`return firstUser;`}</code>
+          <CodeBlock code={mikeNorth} />
         </article>
 
         <article>
           <h4>An interesting side note is that this will work with strings as well:</h4>
-          <code>{`const myfriend = 'jimmy';`}</code>
-          <br />
-          <code>{`const [first, ...rest] = myfriend;`}</code>
-          <br />
-          <code>{`console.log(first);`}</code>
-          <br />
-          <code>{`// 'j'`}</code>
-          <br />
-          <code>{`console.log(rest);`}</code>
-          <br />
-          <code>{`// ['i','m','m','y']`}</code>
+          <CodeBlock code={useString} />
           <br />
           
         </article>
