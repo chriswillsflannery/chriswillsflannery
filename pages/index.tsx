4f1e030411caaa73988142fbccde9fd74e7c2445
Chris Flannery <chriswillsflannery@gmail.com>
2022-10-18 17:03:10 -0400

    constrain generic types

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,12 @@ const Home: NextPage = () => {
         </p>
 
         <p>
+          <Link href="./posts/extendingTsGenerics">
+            {`Oct 18 2022: Constraining TS Generics with "Extends" pattern`}
+          </Link>
+        </p>
+
+        <p>
           <Link href="./posts/crossword1">
             Oct 9 2022: Bilingüe Crossword 1: Tema Perú
           </Link>
