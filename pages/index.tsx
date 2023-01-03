36d8be914ee27eeccfc2eaace5b1358ea77c638c
Chris Flannery <chris.flannery@koneksahealth.com>
2023-01-03 09:13:26 -0500

    snowcrash

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,12 @@ const Home: NextPage = () => {
         {locale==='en-US' ? (
           <>
             <p>
+              <Link href="./posts/snowCrash">
+                {`Snow Crash: Machine Learning & The Future of Research`}
+              </Link>
+            </p>
+
+            <p>
               <Link href="./posts/oop3DataManipulation">
                 {`Object-Oriented Programming in JS: 3. Data Manipulation`}
               </Link>
