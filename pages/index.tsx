40dc63b39c59d8a27e88fa80e7f418ccdd4112d7
Chris Flannery <chrisflannery@pop-os.mynetworksettings.com>
2024-08-26 15:35:29 -0400

    RAG examples batch1

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,12 @@ const Home: NextPage = () => {
         {locale==='en-US' ? (
           <>
             <p>
+              <Link href="./posts/ragExamplesForJobApplication">
+                {`RAG documents for job application autofill`}
+              </Link>
+            </p>
+
+            <p>
               <Link href="./posts/uiStringPatternMatching">
                 {`React: UI string pattern matching with Regex`}
               </Link>
