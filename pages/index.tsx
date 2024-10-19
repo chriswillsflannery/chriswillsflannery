c52eef377d16a6b9d3ac2742ea3f5dac6a44207f
Chris Flannery <chriswillsflannery@gmail.com>
2024-10-19 13:51:01 -0400

    dead internet

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,12 @@ const Home: NextPage = () => {
         {locale==='en-US' ? (
           <>
             <p>
+              <Link href="./posts/deadInternet">
+                {`Dead internet theory is dead!`}
+              </Link>
+            </p>
+
+            <p>
               <Link href="./posts/whyIsJavascriptSingleThreaded">
                 {`Why is JavaScript single threaded? (Go is multi-threaded)`}
               </Link>
