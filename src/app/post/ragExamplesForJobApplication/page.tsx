75910e3cb4fce42c3f9adbbbdcbfd83c2f38b589
Chris Flannery <chriswillsflannery@gmail.com>
2025-02-15 10:58:32 -0500

    more RAG documents

--- src/app/post/ragExamplesForJobApplication/page.tsx
+++ src/app/post/ragExamplesForJobApplication/page.tsx
@@ -121,9 +121,61 @@ export default function Subtitler() {
           <p>{`Next 14 uses Turbopack, which is a next-gen compiler and outperforms webpack on several metrics. It has faster refresh rates, meaning hot-reloading is faster, and we get better build times as well. Next 13 primarily used webpack as a build tool, but later introduced turbopack as an experimental, semi-supported configuration option.`}</p>
           <p>{`The last big change is that Next 14 encourages the use of Server Components. This involves reviewing all components and deciding which can be server-rendered and which must be client only. In effect, this creates a 3-tiered rendering system where server-rendered HTML is compiled first, and populates the "bones" of the webpage, then server components get dropped in, and then finally, client components. This attempts to create an extremely buttery user experience, where it doesn't really "feel" like you have to wait for things to become interactive.`}</p>
         </article>
-      </main>
+        <HL />
 
-      <HL />
+        <article>
+          <h3>{`What's the most ambitious project you've built in a weekend? What drove you to build it?`}</h3>
+          <p>{`Recently I became interested in how tiktok and instagram automatically generate subtitles based on the audio of the video you upload. Due to a mistake in my own understanding about the platforms, I went deep down the rabbit hole trying to understand how audio models like Google USM and AWS Transcribe work. I wrote about it here: https://chriswillsflannery.vercel.app/posts/autosubtitler`}</p>
+          <p>{`In a weekend, I built a prototype for an automatic subtitler pipeline in AWS. I used findings from the Google whitepaper mentioned in my blog post to inform the decision-making process. Currently, I can upload a video to s3 on my phone, and it will spit out the same video with subtitles burned on. It's pretty cheap to run (a single Lambda process on a cold start) and was a lot of fun to learn about FFmpeg and AWS Transcribe.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`Share a story about when you broke something significant in production. What did you learn?`}</h3>
+          <p>{`I haven't had the pleasure of breaking production yet, but during my first software dev role, I distinctly remember 2 times nearly in succession accidentally exposing my API keys in a git commit. I wasn't sure what to do - do I just scrub the git history? I told my manager and he was totally understanding, but basically just told me "go figure out how to fix this, take an hour or two and if you can't figure it out give me a shout. Work fast, this is a scenario where it's important to get this fixed quickly". Mind you, this was before LLMs so I had to read a ton of documentation, and reach out to a devops person to collaborate on the fix.`}</p>
+          <p>{`This experience was a little scary, definitely high pressure, but taught me 2 very important lessons: 1. about application secrets/credentials rotation and why that's important - and was a little embarrassing too. But I think often those types of scenarios are the best ways to really make the high level important stuff stick. 2. About why it's important to know your team and build a "knowledge graph" of the people you work with. It's a good practice to get an idea of what everyone typically works on and what they're good at, and what they can help you with. Had I made an intro to that devops person earlier (thus knowing earlier in the process that I could go to them with my problem), I may have shaved a good 15-20 minutes off of the fix process.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`Outside of standard web frameworks, what's the most interesting technology you've taught yourself recently? Why did you choose to learn it?`}</h3>
+          <p>{`For a few years I was in a touring band playing drums, and these days I don't have a full drum kit anymore (apartment life) but I recently picked up a Roland TD27KV2 kit and have been playing around with sound modeling - in particular, around trying to reproduce as best as possible the sounds (tone, timbre) and feel (gating, threshold, volume) of various real kits.`}</p>
+          <p>{`It's a good practice to still scratch the technical/creative itch while giving my eyes a break and putting my ears to work.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`Without any prior RAG experience, how would you approach building a RAG pipeline for financial documents? What questions would you ask?`}</h3>
+          <p>{`First I would want to understand the different data sources at our disposal. What is the structured data? What is unstructured data? Are we using any nontraditional data sources like scraping social media or news?`}</p>
+          <p>{`Do the data sources we have, best lend themselves to traditional RAG or is there a graph-entity relationship in the data which we can leverage? For example, in the legal world, it's often the case that one case law paper will reference other case law papers and we can leverage this graph structure. Is there something similar in financial documents - one document references other documents or document owners? Then we can explore things like graph RAG, or graph RAG alternatives like Light RAG (which on average outperform traditional graphRAG on a number of metrics).`}</p>
+          <p>{`In an extremely distilled arbitrary example I could imagine we might want to do something like:`}</p>
+          <p>{`1. An end-user queries a chatbot to ask about their tax implications for the next year.`}</p>
+          <p>{`2. The user's query is sent with a system prompt to a small distill model to determine which of several vector databases to hit, to augment the query to an LLM, with either information about the user's spending history, or some similar internal/proprietary data.`}</p>
+          <p>{`3. Chunked vector data is appropriately retrieved and added to the context of a query to an LLM to forecast the answer to the user's question, and the answer is displayed as a response in the chat UI.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`What's your philosophy on technical debt? When do you embrace it, and when do you fight it?`}</h3>
+          <p>{`The absolute number 1 most important thing for a company should be delivering a product that users like, that works well for users, and that users enjoy. Everything else should flow downstream from there. At the beginning, a company should be squarely focused on delivering customer value. They should hire incredible early contributors who have been through the ringer and know how to deliver future-proofed solutions which help the company ship fast and ship often. Of course, some things will slip through the cracks. That's the nature of software. It's inevitable to some degree. Early on though, it's important not to get sidetracked. Focus on tech debt too early and runway will run out. In My Humble Opinion you can temper the results of bad tech debt accumulation by making smart hiring decisions early. When the company grows larger, you can take a minute to breathe and reflect and allocate some people resources to cleanup.`}</p>
+          <p>{`One thing to maybe consider here is that tech debt buildup is often a result of either poor planning/deadlines, changing requirements, and knowledge gaps. LLMs are largely helping us close the gap on all of these things. They are helping us deliver products faster than ever, and understand where the best value-adds might be in a system. Something I feel is incredibly important in startup world is to "do it the right way the first time". Maybe we can shave some days off delivery by rolling out a lightweight MVP to probe user interest/market value. But we shouldn't take shortcuts in system considerations where it matters - for example, don't roll out a RBAC permissioning for proprietary data/information unless you're 100% sure it's working as expected, has been thoroughly tested across all roles, etc. Use good judgment. That HAS to be done right the first time. And my point here is, maybe in the past, a consideration like that may have set a team's schedule back a few days or weeks. Now, with copilot tools we can ship it fast, and right, the first time.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`You discover a potential 10x improvement in our system's performance, but it requires a complete rewrite of a critical component. It's month-end, and users are relying on the current system. What's your play?`}</h3>
+          <p>{`Hackathon. Get it done. Document everything and build as PoC in isolation. Extensively test, even A-B test with a small group of company friends/power users to ensure functionality. Feature flag. Ship and monitor. If possible, it would be good to (blue-green deploy) build parallel processes and gradually migrate real users to the new process (for example, after usual user hours). Rollback contingency plan also involves the ability to gradually migrate users back to stable platform.`}</p>
+        </article>
+        <HL />
+
+        <article>
+          <h3>{`If you had unlimited resources and no constraints, what would you build?`}</h3>
+          <p>{`Are we saying that time is not a constraint? I would probably become interested in building space civilization/governance and helping develop new worlds.`}</p>
+          <p>{`If time is a constraint and I had unlimited power and money, I would probably be most interested in renewable energy/natural resource conservation on earth and involvement in Mars planning. I am also interested in consciousness and neuron-mapping like we see in the show "Pantheon". Longevity science is tangential and also fascinating. As AI becomes more and more advanced, I find myself becoming more interested in exploring what it is that makes humans - human. Some of my favorite human things in the world are: a cup of coffee and conversation with my family, a book and a quiet spot in the woods by the lake, and surfing. I would like to see these things stand the test of time.`}</p>
+        </article>
+        <HL />
+
+      </main>
     </>
   );
 }
